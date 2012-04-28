faaab7da906c32c77550c950a0a785da05e3a631
gradus <kriswindham@gmail.com>
2012-04-27 22:16:00 -0400

    the mothership has landed

--- examples/server.js
+++ examples/server.js
@@ -1,1 +1,14 @@
 saucer = require('../bin/saucer')
+pin = require('linchpin')
+apprentice = require('apprentice')
+
+pin.on('GET', function(req, res) {
+  res.writeHead(200, {'content-type': 'text/html'});
+  res.end('<strong>The Mothership has landed</strong>');
+});
+
+
+apprentice.httpServer.listen(3000);
+
+
+
